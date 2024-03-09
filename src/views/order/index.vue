56ef683207b8ffc6240b5b8591d2267f850203ff
David <844332239@qq.com>
2024-03-09 20:13:39 +0800

    产品管理和订单管理页面布局优化

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -1,26 +1,67 @@
 <template>
-  <el-table :data="tableData" stripe border fit max-height="85vh" style="width: 100%; overflow-y: auto;">
-    <el-table-column show-overflow-tooltip="true" align="center" type="selection" width="50"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="index" label="序号" width="60" fixed="left"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="orderNumber" label="订单号" width="180" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productTotalPrice" label="商品总价" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="freight" label="订单运费" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="actualPayment" label="实付款" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="paymentWay" label="支付方式" width="120" sortable>
-      <template #default="scope">
-        <span>{{scope.row.paymentWay === 1 ? '微信' : scope.row.paymentWay === 2 ? '支付宝' : '银行卡'}}</span>
-      </template>
-    </el-table-column>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="createdTime" label="下单时间" width="160" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="paymentTime" label="付款时间" width="160" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="shippingTime" label="发货时间" width="160" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" fixed="right" label="操作" width="120">
-      <template #default>
-        <el-button link type="primary" size="small">详情</el-button>
-        <el-button link type="primary" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div style="height: 80vh">
+    <el-container>
+      <el-header>
+        <el-row :gutter="10" style="margin-top: 3px">
+          <el-col :span="4">
+            <el-input size="small" placeholder="订单号"></el-input>
+          </el-col>
+          <el-col :span="8">
+            <el-date-picker
+                v-model="times"
+                type="datetimerange"
+                range-separator="到"
+                size="small"
+                start-placeholder="Start date"
+                end-placeholder="End date"
+            />
+          </el-col>
+          <el-col :span="4">
+            <el-button size="small" type="primary">查询</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-table :data="tableData" stripe border fit max-height="80vh" style="width: 100%; overflow-y: auto;">
+          <el-table-column align="center" type="selection" width="50"/>
+          <el-table-column align="center" prop="index" label="序号" width="60" fixed="left"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="orderNumber" label="订单号" width="180" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productTotalPrice" label="商品总价" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="freight" label="订单运费" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="actualPayment" label="实付款" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="paymentWay" label="支付方式" width="120" sortable>
+            <template #default="scope">
+              <span>{{scope.row.paymentWay === 1 ? '微信' : scope.row.paymentWay === 2 ? '支付宝' : '银行卡'}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="createdTime" label="下单时间" width="160" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="paymentTime" label="付款时间" width="160" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="shippingTime" label="发货时间" width="160" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" fixed="right" label="操作" width="120">
+            <template #default>
+              <el-button link type="primary" size="small">详情</el-button>
+              <el-button link type="primary" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+      <el-footer>
+        <el-pagination
+            v-model:current-page="currentPage"
+            v-model:page-size="pageSize"
+            :page-sizes="[50, 100, 200, 300]"
+            :small="small"
+            :disabled="disabled"
+            :background="background"
+            style="margin-left: 10px; margin-top: 3px"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="tableData.length"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
+      </el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -46,8 +87,40 @@ function generatorOrderList():void {
 }
 generatorOrderList();
 
+const times = ref<[Date, Date]>([
+  new Date(2024, 3, 1, 0, 0, 0),
+  new Date(2024, 3, 15, 23, 59, 59),
+]);
+
+const currentPage = ref(1);
+const pageSize = ref(50);
+const small = ref(false);
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 </script>
 
 <style scoped lang="scss">
-
+.el-header {
+  --el-header-padding: 0;
+  --el-header-height: 4vh;
+  padding: var(--el-header-padding);
+  height: var(--el-header-height);
+}
+.el-main {
+  --el-main-padding: 0;
+  padding: var(--el-main-padding);
+}
+.el-footer {
+  --el-footer-padding: 0;
+  --el-footer-height: 4vh;
+  padding: var(--el-footer-padding);
+  height: var(--el-footer-height);
+}
 </style>
--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -1,49 +1,93 @@
 <template>
-  <el-table :data="tableData" stripe border fit max-height="85vh" style="width: 100%; overflow-y: auto;">
-    <el-table-column show-overflow-tooltip="true" align="center" type="selection" width="50"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="index" label="序号" width="60" fixed="left"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productNumber" label="商品编码" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productName" label="商品名称" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productBrandName" label="商品品牌" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productCategoryName" label="商品分类" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="productPicture" label="商品图片" width="120"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="publishStatus" label="上架状态" width="120" sortable>
-      <template #default="scope">
-        <el-tag :type="scope.row.publishStatus === 1 ? 'success':'warning'">
-          <span>{{ scope.row.publishStatus === 1 ? '已上架' : '未上架' }}</span>
-        </el-tag>
-      </template>
-    </el-table-column>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="newProductStatus" label="是否新品" width="120" sortable>
-      <template #default="scope">
-        <el-tag :type="scope.row.newProductStatus === 1 ? 'success':'warning'">
-          <span>{{ scope.row.newProductStatus === 1 ? '新品' : '非新品' }}</span>
-        </el-tag>
-      </template>
-    </el-table-column>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="price" label="商品价格" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="promotionPrice" label="促销价格" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="giftPoints" label="赠送积分" width="120"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="description" label="商品描述" width="120"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="stock" label="商品库存" width="120" sortable>
-      <template #default="scope">
-        <span :style="{'color': scope.row.stock < 110 ? 'red' : ''}">{{ scope.row.stock }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="weight" label="商品重量" width="120" sortable/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="promotionStartTime" label="促销开始时间" width="160"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="promotionEndTime" label="促销结束时间" width="160"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="createdBy" label="创建人" width="120"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="createdTime" label="创建时间" width="160"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="updatedBy" label="修改人" width="120"/>
-    <el-table-column show-overflow-tooltip="true" align="center" prop="updatedTime" label="修改时间" width="160"/>
-    <el-table-column show-overflow-tooltip="true" align="center" fixed="right" label="操作" width="120">
-      <template #default>
-        <el-button link type="primary" size="small">详情</el-button>
-        <el-button link type="primary" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div style="height: 80vh">
+    <el-container>
+      <el-header>
+        <el-row :gutter="10" style="margin-top: 3px">
+          <el-col :span="4">
+            <el-input size="small" placeholder="商品编码"></el-input>
+          </el-col>
+          <el-col :span="4">
+            <el-input size="small" placeholder="商品名称"></el-input>
+          </el-col>
+          <el-col :span="8">
+            <el-date-picker
+                v-model="times"
+                type="datetimerange"
+                range-separator="到"
+                size="small"
+                start-placeholder="Start date"
+                end-placeholder="End date"
+            />
+          </el-col>
+          <el-col :span="4">
+            <el-button size="small" type="primary">查询</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-table :data="tableData" stripe border fit max-height="80vh" style="width: 100%; overflow-y: auto;">
+          <el-table-column align="center" type="selection" width="50"/>
+          <el-table-column align="center" prop="index" label="序号" width="60" fixed="left"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productNumber" label="商品编码" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productName" label="商品名称" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productBrandName" label="商品品牌" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productCategoryName" label="商品分类" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="productPicture" label="商品图片" width="120"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="publishStatus" label="上架状态" width="120" sortable>
+            <template #default="scope">
+              <el-tag :type="scope.row.publishStatus === 1 ? 'success':'warning'">
+                <span>{{ scope.row.publishStatus === 1 ? '已上架' : '未上架' }}</span>
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="newProductStatus" label="是否新品" width="120" sortable>
+            <template #default="scope">
+              <el-tag :type="scope.row.newProductStatus === 1 ? 'success':'warning'">
+                <span>{{ scope.row.newProductStatus === 1 ? '新品' : '非新品' }}</span>
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="price" label="商品价格" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="promotionPrice" label="促销价格" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="giftPoints" label="赠送积分" width="120"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="description" label="商品描述" width="120"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="stock" label="商品库存" width="120" sortable>
+            <template #default="scope">
+              <span :style="{'color': scope.row.stock < 110 ? 'red' : ''}">{{ scope.row.stock }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="weight" label="商品重量" width="120" sortable/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="promotionStartTime" label="促销开始时间" width="160"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="promotionEndTime" label="促销结束时间" width="160"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="createdBy" label="创建人" width="120"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="createdTime" label="创建时间" width="160"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="updatedBy" label="修改人" width="120"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" prop="updatedTime" label="修改时间" width="160"/>
+          <el-table-column :show-overflow-tooltip="true" align="center" fixed="right" label="操作" width="120">
+            <template #default>
+              <el-button link type="primary" size="small">详情</el-button>
+              <el-button link type="primary" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+      <el-footer>
+        <el-pagination
+            v-model:current-page="currentPage"
+            v-model:page-size="pageSize"
+            :page-sizes="[50, 100, 200, 300]"
+            :small="small"
+            :disabled="disabled"
+            :background="background"
+            style="margin-left: 10px; margin-top: 3px"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="tableData.length"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
+      </el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -79,8 +123,41 @@ function generatorProductList():void {
   }
 }
 generatorProductList();
+
+const times = ref<[Date, Date]>([
+  new Date(2024, 3, 1, 0, 0, 0),
+  new Date(2024, 3, 15, 23, 59, 59),
+]);
+
+const currentPage = ref(1);
+const pageSize = ref(50);
+const small = ref(false);
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 </script>
 
 <style scoped lang="scss">
-
+.el-header {
+  --el-header-padding: 0;
+  --el-header-height: 4vh;
+  padding: var(--el-header-padding);
+  height: var(--el-header-height);
+}
+.el-main {
+  --el-main-padding: 0;
+  padding: var(--el-main-padding);
+}
+.el-footer {
+  --el-footer-padding: 0;
+  --el-footer-height: 4vh;
+  padding: var(--el-footer-padding);
+  height: var(--el-footer-height);
+}
 </style>
